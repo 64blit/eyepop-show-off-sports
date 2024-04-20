82468f67806df9a12d91bcaca12abe7383edc2e4
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-04-19 18:26:12 -0700

    Fixed the bounds expansion to include the sports ball for all players

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ def main(video_file_path: str, target_jersey_number: str, analyze=False, smoothi
         #
         #  1. iterate through the eyepop results and add the people to the person tracker
         #
+        source_width = 0
+        source_height = 0
         for result in json_data:
 
             source_width = result['source_width']
@@ -82,38 +84,41 @@ def main(video_file_path: str, target_jersey_number: str, analyze=False, smoothi
 
                 # expand the bounds of the person to contain the sports ball location
                 if sports_ball_location['x'] != -1:
-                    diff_x = abs(sports_ball_location['x'] + obj['x'])
-                    diff_y = abs(sports_ball_location['y'] + obj['y'])
 
-                    obj['x'] = min(obj['x'], sports_ball_location['x'])
-                    obj['y'] = min(obj['y'], sports_ball_location['y'])
+                    # Calculate new bounding box coordinates and dimensions
+                    new_min_x = min(obj['x'], sports_ball_location['x'])
+                    new_max_x = max(obj['x'] + obj['width'],
+                                    sports_ball_location['x'] + sports_ball_location['width'])
+                    new_min_y = min(obj['y'], sports_ball_location['y'])
+                    new_max_y = max(obj['y'] + obj['height'],
+                                    sports_ball_location['y'] + sports_ball_location['height'])
 
-                    obj['width'] = obj['width'] + \
-                        sports_ball_location['width'] + \
-                        diff_x
+                    obj['x'] = new_min_x
+                    obj['y'] = new_min_y
+                    obj['width'] = new_max_x - new_min_x
+                    obj['height'] = new_max_y - new_min_y
 
-                    obj['height'] = obj['height'] + \
-                        sports_ball_location['height'] + \
-                        diff_y
-
-                # calculate the distance between the person and the ball in %
+                # Calculate the normalized distance between the person and the ball
                 if sports_ball_location['x'] != -1:
-                    x1 = (
-                        sports_ball_location['x'] + (sports_ball_location['width'] / 2)) / source_width
-                    y1 = (
-                        sports_ball_location['y'] + (sports_ball_location['height'] / 2)) / source_height
+                    # Calculate normalized center coordinates of the sports ball
+                    x1 = (sports_ball_location['x'] +
+                          (sports_ball_location['width'] / 2)) / source_width
+                    y1 = (sports_ball_location['y'] +
+                          (sports_ball_location['height'] / 2)) / source_height
 
+                    # Calculate normalized center coordinates of the object (person)
                     x2 = (obj['x'] + (obj['width'] / 2)) / source_width
                     y2 = (obj['y'] + (obj['height'] / 2)) / source_height
 
-                    ball_distance = abs(((x1 - x2)**2 + (y1 - y2)**2)**0.5)
+                    # Euclidean distance between the two normalized points
+                    ball_distance = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
 
                 # if there is no trace id, we ignore the person
-                if (trace_id == None):
+                if (trace_id == None and labels == []):
                     continue
 
                 # # if the ball distance is 20% or more of the screen width, we ignore the person
-                # if ball_distance > 0.6 or ball_distance == -1:
+                # if ball_distance > 0.4 or ball_distance == -1:
                 #     continue
 
                 # add the person to the person tracker
@@ -126,7 +131,7 @@ def main(video_file_path: str, target_jersey_number: str, analyze=False, smoothi
                 )
 
         # filter and consolidate the people in the person tracker
-        person_tracker.filter_map()
+        person_tracker.filter_map(source_width, source_height, threshold=2)
 
         if (debug):
             # print all the keys in the person tracker
@@ -156,7 +161,7 @@ def main(video_file_path: str, target_jersey_number: str, analyze=False, smoothi
             time.sleep(1)
 
             mm.create_video(video_file_path, file_name,
-                            person['time_segments'], person['bounds'], resolution=(720, 800), draw_bounds=draw_bounds)
+                            person['time_segments'], person['bounds'], resolution=(720, 480), draw_bounds=draw_bounds)
 
     upload_video(video_file_path)
 
@@ -167,7 +172,7 @@ args = ap.ArgumentParser()
 args.add_argument("--video", type=str, default='./Video.MOV')
 args.add_argument("--target", type=str, default=None, nargs='?')
 args.add_argument("--analyze", action="store_true")
-args.add_argument("--smoothing", type=float, default=.99, nargs='?')
+args.add_argument("--smoothing", type=float, default=.95, nargs='?')
 args.add_argument("--draw_bounds", action="store_true")
 args.add_argument("--debug", action="store_true")
 args = args.parse_args()
